5eb024e1c7ccb4653bbeb64e28c275848e53dcbc
Yufan Liu <liuyufansszx@hotmail.com>
2015-09-30 15:15:50 -0700

    java version for realtime collaborative web services

---
+++ vertx-java/src/main/java/vertx/playground/java/draw/Server.java
@@ -0,0 +1,47 @@
+package vertx.playground.java.draw;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+
+import vertx.playground.java.util.Runner;
+
+public class Server extends AbstractVerticle {
+
+	  // Convenience method so you can run it in your IDE
+	  public static void main(String[] args) {
+	    Runner.runExample(Server.class);
+	  }
+
+	  @Override
+	  public void start() throws Exception {
+
+	    Router router = Router.router(vertx);
+
+	    // Allow events for the designated addresses in/out of the event bus bridge
+	    BridgeOptions opts = new BridgeOptions()
+	      .addInboundPermitted(new PermittedOptions().setAddress("draw"))
+	      .addOutboundPermitted(new PermittedOptions().setAddress("draw"));
+
+	    // Create the event bus bridge and add it to the router.
+	    SockJSHandler ebHandler = SockJSHandler.create(vertx).bridge(opts);
+	    router.route("/eventbus/*").handler(ebHandler);
+
+	    // Create a router endpoint for the static content.
+	    router.route().handler(StaticHandler.create());
+
+	    // Start the web server and tell it to use the router to handle requests.
+	    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+
+	  }
+	}
